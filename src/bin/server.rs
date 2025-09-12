c254ed0454e58597be93934c1048f5c9798f743e
Kat <katthegit@gmail.com>
2025-09-13 01:59:23 +0200

    timer back to 10s

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -371,7 +371,7 @@ fn main() {
 
       // Occasionally check for goners
       for player_index in 0..listener_players.len() {
-        if (listener_players[player_index].last_packet_time.elapsed().as_secs_f32() > 100.0)
+        if (listener_players[player_index].last_packet_time.elapsed().as_secs_f32() > 10.0)
         && (listener_players[player_index].character != Character::Dummy                  ) {
           listener_players.remove(player_index);
           break;
