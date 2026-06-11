38dd2811c9f50175ed33ed9266395351435a67a5
KatTheGit <katthegit@gmail.com>
2026-06-11 18:04:55 +0200

    add tiled tileset

---
+++ assets/tiles.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="asdf" tilewidth="200" tileheight="400" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="gameobjects/wall.png" width="200" height="400"/>
+ </tile>
+ <tile id="1">
+  <image source="gameobjects/grass-1.png" width="200" height="200"/>
+ </tile>
+ <tile id="2">
+  <image source="gameobjects/grass-1-b.png" width="200" height="200"/>
+ </tile>
+ <tile id="3">
+  <image source="gameobjects/unbreakable_wall.png" width="200" height="400"/>
+ </tile>
+ <tile id="4">
+  <image source="gameobjects/pointtemp.png" width="200" height="200"/>
+ </tile>
+ <tile id="5">
+  <image source="gameobjects/spawnpoint_blue.png" width="200" height="200"/>
+ </tile>
+ <tile id="6">
+  <image source="gameobjects/spawnpoint_red.png" width="200" height="200"/>
+ </tile>
+</tileset>
