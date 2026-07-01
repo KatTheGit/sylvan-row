9700c106f664d514ded603a174da180574f3d30a
KatTheGit <katthegit@gmail.com>
2026-07-01 17:36:11 +0200

    Fix compilation script

--- compile-pc.sh
+++ compile-pc.sh
@@ -6,17 +6,22 @@ mv target/x86_64-pc-windows-gnu/release/sylvan_row.exe target/sr_windows/run.exe
 # assets
 cp -r assets target/sr_windows/assets
 find target/sr_windows/assets -type f -name "*.kra" -delete
-zip -r target/sr_windows.zip target/sr_windows
-
+cd target
+rm sr_windows.zip
+zip -r sr_windows.zip sr_windows
+cd ..
 # build for Linux
 mkdir target/sr_linux
 rm -rf target/sr_linux/*
 cargo build --bin sylvan_row --release --target x86_64-unknown-linux-gnu
-mv target/x86_64-unknown-linux-gnu/release/sylvan_row target/sr_windows/run
+mv target/x86_64-unknown-linux-gnu/release/sylvan_row target/sr_linux/run
 # assets
-cp -r assets target/sr_windows/assets
+cp -r assets target/sr_linux/assets
 find target/sr_linux/assets -type f -name "*.kra" -delete
-zip -r target/sr_linux.zip target/sr_linux
+cd target
+rm sr_linux.zip
+zip -r sr_linux.zip sr_linux
+cd ..
 
 # build the server
 cargo build --bin server --release --target x86_64-unknown-linux-gnu
